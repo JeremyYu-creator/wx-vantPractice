0437b6e210c440da7aef9f760c62adafe1c02265
yufei <970038661@qq.com>
2022-04-18 16:31:26 +0800

    feat: 增加部分内容

--- miniprogram/pages/logs/logs.ts
+++ miniprogram/pages/logs/logs.ts
@@ -30,6 +30,45 @@ Page({
     // })
     console.log(e.detail)
   },
+  onReady() {
+    const query = wx.createSelectorQuery() // 获取dom结点
+    query.select('#circle')
+      .fields({node: true, size: true})
+      .exec((res: any) => {
+        const canvas = res[0].node
+        const ctx = canvas.getContext('2d')// 设置为2d的实例
+        const dpr = wx.getSystemInfoSync().pixelRatio // 获取到自适应屏幕的单位
+        canvas.width = 300 * dpr
+        canvas.height = 150 * dpr
+        ctx.scale(dpr, dpr)
+        ctx.fillStyle = "rgb(200,0,0)"
+        console.log(res)
+        // ctx.fillRect(0, 0, 50, 50)
+        // ctx.strokeRect(20, 20, 10, 10)
+        // ctx.fillRect(100, 100, 200, 200)
+        // ctx.strokeRect(200, 100, 30, 50)
+        // ctx.beginPath()
+        // ctx.moveTo(50, 50)
+        // ctx.lineTo(200, 100)
+        // ctx.closePath()
+        // ctx.stroke()
+        // ctx.beginPath()
+        // ctx.moveTo(100, 100)
+        // ctx.lineTo(100, 50)
+        // ctx.lineTo(50,50)
+        // ctx.closePath()
+        // ctx.fill()
+        ctx.beginPath()
+        ctx.arc(100, 100, 25, 0, Math.PI * 2, false)
+        ctx.stroke()
+        ctx.fill()
+        // ctx.beginPath()
+        // ctx.moveTo(0, 0)
+        // ctx.arcTo(100, 50, 100, 100, 50);
+        // ctx.closePath()
+        // ctx.stroke()
+      })
+},
   onLoad() {
     this.setData({
       logs: (wx.getStorageSync('logs') || []).map((log: string) => {
